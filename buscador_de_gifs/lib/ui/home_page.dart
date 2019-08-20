bbe941584feaec3c7ada1bcd0a012002e4a322be
VagnerSabadi <vagnerbuzatta@hotmail.com>
2019-08-19 22:36:13 -0300

    start front buscador de gifs

---
+++ buscador_de_gifs/lib/ui/home_page.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+String _search;
+int _offSet = 0;
+
+Future<Map> _getGifs() async {
+  http.Response response;
+  if(_search == null){
+    response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=Uk6N4REnLoBPzspmJQGlEND7KXDDIux5&limit=20&rating=G");
+  }else{
+    response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=Uk6N4REnLoBPzspmJQGlEND7KXDDIux5&q=$_search&limit=20&offset=$_offSet&rating=G&lang=en");    
+  }
+  return json.decode(response.body);
+}
+
+@override
+  void initState() {
+    super.initState();
+    _getGifs().then((map){
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     appBar: AppBar(
+      backgroundColor: Colors.black,
+      title: Image.network("https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+      centerTitle: true,
+     ),  
+     backgroundColor: Colors.black,
+     body: Column(
+       children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(10.0),
+          child:  TextField(
+           decoration: InputDecoration(
+             labelText: "Pesquise aqui!",
+             labelStyle: TextStyle(color: Colors.white),
+             border: OutlineInputBorder()
+             ),
+             style: TextStyle(color: Colors.white, fontSize: 18.0),
+             textAlign: TextAlign.center,
+         ),
+        )
+       ],
+     ),    
+    );
+  }
+}
