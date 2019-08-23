71503ac7accfe53499b16c038df53904446e71b9
VagnerSabadi <vagnerbuzatta@hotmail.com>
2019-08-22 22:26:41 -0300

    Finalizado buscado e gifs

--- buscador_de_gifs/lib/ui/home_page.dart
+++ buscador_de_gifs/lib/ui/home_page.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'package:buscador_de_gifs/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -13,12 +16,12 @@ class _HomePageState extends State<HomePage> {
 
   Future<Map> _getGifs() async {
     http.Response response;
-    if (_search == null) {
+   if(_search == null || _search.isEmpty) {
       response = await http.get(
           "https://api.giphy.com/v1/gifs/trending?api_key=Uk6N4REnLoBPzspmJQGlEND7KXDDIux5&limit=20&rating=G");
     } else {
       response = await http.get(
-          "https://api.giphy.com/v1/gifs/search?api_key=Uk6N4REnLoBPzspmJQGlEND7KXDDIux5&q=$_search&limit=20&offset=$_offSet&rating=G&lang=en");
+          "https://api.giphy.com/v1/gifs/search?api_key=Uk6N4REnLoBPzspmJQGlEND7KXDDIux5&q=$_search&limit=19&offset=$_offSet&rating=G&lang=en");
     }
     return json.decode(response.body);
   }
@@ -50,9 +53,10 @@ class _HomePageState extends State<HomePage> {
                   border: OutlineInputBorder()),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
-              onSubmitted: (String text){
+              onSubmitted: (String text) {
                 setState(() {
-                _search = text;                  
+                  _search = text;
+                  _offSet = 0;
                 });
               },
             ),
@@ -87,21 +91,61 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  int _getCount(List data) {
+    if (_search == null) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
 //criar grid com gifs
   _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
     return GridView.builder(
         padding: EdgeInsets.all(10.0),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
-        itemCount: snapshot.data["data"].length,
+        itemCount: _getCount(snapshot.data["data"]),
         itemBuilder: (context, index) {
-          return GestureDetector(
-            child: Image.network(
-              snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+          if (_search == null || index < snapshot.data["data"].length) {
+            return GestureDetector(
+              child: FadeInImage.memoryNetwork(image: snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+              placeholder: kTransparentImage,
               height: 300.0,
               fit: BoxFit.cover,
-            ),
-          );
+              ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            GifPage(snapshot.data["data"][index])));
+              },
+              onLongPress: (){
+                Share.share( snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
+              },
+            );
+          } else {
+            return Container(
+              child: GestureDetector(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.add, color: Colors.white, size: 70.0),
+                    Text(
+                      "Carregar mais...",
+                      style: TextStyle(color: Colors.white, fontSize: 22.0),
+                    )
+                  ],
+                ),
+                onTap: () {
+                  setState(() {
+                    _offSet += 19;
+                  });
+                },
+              ),
+            );
+          }
         });
   }
 }
