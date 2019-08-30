6b8f2abc10a2d552841cf94c3ec261ab50cec89a
VagnerSabadi <vagnerbuzatta@hotmail.com>
2019-08-29 23:42:23 -0300

    remove and add code course

--- agenda_de_contatos/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:agenda_de_contatos/ui/home_page.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-    debugShowCheckedModeBanner: false,
-  ));
-}
