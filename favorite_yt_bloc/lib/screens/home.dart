4cdc149110e03d3e3b2c75a0b5be07d5bbef9233
VagnerSabadi <vagnerbuzatta@hotmail.com>
2019-09-05 22:17:36 -0300

    finish search

--- favorite_yt_bloc/lib/screens/home.dart
+++ favorite_yt_bloc/lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_yt_bloc/delegates/data_search.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -6,7 +7,34 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: null,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Container(
+            height: 25,
+            child: Image.asset("images/yt_logo_rgb_dark.png"),
+          ),
+          elevation: 0,
+          backgroundColor: Colors.black87,
+          actions: <Widget>[
+            Align(
+              alignment: Alignment.center,
+              child: Text("0"),
+            ),
+            IconButton(
+              icon: Icon(Icons.star),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () async {
+              String result = await  showSearch(context: context, delegate: DataSearch());
+              
+              },
+            ),
+          ],
+        ),
+        body: Container(),
+      ),
     );
   }
 }
