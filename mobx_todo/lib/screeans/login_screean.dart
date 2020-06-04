bb8b76484dd0ca6b39426e7159d209a7245b86b8
Vagner Sabadi <vagner@odig.net>
2020-06-04 20:41:24 -0300

    feat(): create project

---
+++ mobx_todo/lib/screeans/login_screean.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginScreean extends StatefulWidget {
+  LoginScreean({Key key}) : super(key: key);
+
+  @override
+  _LoginScreeanState createState() => _LoginScreeanState();
+}
+
+class _LoginScreeanState extends State<LoginScreean> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
